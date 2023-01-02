5a718cac81efbc350471da310fc30ae9b7712a20
miles <1290672153@qq.com>
2023-01-02 11:38:14 +0800

    20230102

--- src/com/company/leetcode/Test205同构字符串.java
+++ src/com/company/leetcode/Test205同构字符串.java
@@ -1,5 +1,7 @@
 package com.company.leetcode;
 
+import java.util.HashMap;
+
 /**
  * title:
  *
@@ -8,8 +10,26 @@ package com.company.leetcode;
  */
 public class Test205同构字符串 {
     public static void main(String[] args) {
-
+        String s = "badc", t = "baba";
+        System.out.println(isIsomorphic(s, t) && isIsomorphic(t, s));
     }
 
+    public static boolean isIsomorphic(String s, String t) {
+        return help(s, t) && help(t, s);
+    }
 
+    private static boolean help (String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+        // 将两个字符串s和t建立对应关系
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), t.charAt(i));
+        }
+        // 再次遍历第一个字符串s，如果发现map所get()出来的字符与字符串t所指的字符与不一致时，就返回false
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
